c8f2cb7829e550ee70e36c9ffb6c09e365bd1b89
Roman Pinyazhin <pinyazhin@haulmont.com>
2017-12-08 12:08:12 +0400

    PL-8226 Enable noopener and noreferrer modes for Links with target=_blank by default

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/link/CubaLinkWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/link/CubaLinkWidget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2016 Haulmont.
+ * Copyright (c) 2008-2017 Haulmont.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,25 +12,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-package com.haulmont.cuba.gui.components;
+package com.haulmont.cuba.web.toolkit.ui.client.link;
 
-/**
- * HTML link component
- *
- */
-public interface Link extends Component, Component.HasCaption, Component.BelongToFrame, Component.HasIcon {
+import com.google.gwt.dom.client.Element;
+import com.vaadin.client.ui.VLink;
 
-    String NAME = "link";
+public class CubaLinkWidget extends VLink {
 
-    void setUrl(String url);
-    String getUrl();
-
-    void setTarget(String target);
-    String getTarget();
-
-    void setRel(String rel);
-    String getRel();
+    public void setRel(String rel) {
+        Element element = getElement().getFirstChildElement();
+        element.setAttribute("rel", rel);
+    }
 }
--- modules/web/src/com/haulmont/cuba/web/toolkit/ui/client/link/CubaLinkState.java
+++ modules/web/src/com/haulmont/cuba/web/toolkit/ui/client/link/CubaLinkState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2016 Haulmont.
+ * Copyright (c) 2008-2017 Haulmont.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,25 +12,15 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-package com.haulmont.cuba.gui.components;
+package com.haulmont.cuba.web.toolkit.ui.client.link;
 
-/**
- * HTML link component
- *
- */
-public interface Link extends Component, Component.HasCaption, Component.BelongToFrame, Component.HasIcon {
+import com.vaadin.shared.annotations.NoLayout;
+import com.vaadin.shared.ui.link.LinkState;
 
-    String NAME = "link";
+public class CubaLinkState extends LinkState {
 
-    void setUrl(String url);
-    String getUrl();
-
-    void setTarget(String target);
-    String getTarget();
-
-    void setRel(String rel);
-    String getRel();
+    @NoLayout
+    public String rel = "noopener noreferrer";
 }
